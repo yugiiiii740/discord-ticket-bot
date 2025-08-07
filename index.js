d23966e9219ab99d49ce1ecbc193e1725f3d5d71
yugiiiii740 <yugi3436@gmail.com>
2025-08-07 13:17:55 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("fs");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+// Komutları yükle
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+// Eventleri yükle
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+const config = require('./config.json');
+client.login(config.token);
